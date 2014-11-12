a5d3bafc92a1e4a156f6155a42882f1892d4ef24
Lawrence Ma <lawrencema@lawrences-air.att.net>
2014-11-12 01:04:06 -0800

    adds written content to books section

--- app/assets/javascripts/opacity.js
+++ app/assets/javascripts/opacity.js
@@ -1,10 +1,18 @@
  $(document).ready(function() {
-      if (screen.width > 767) {
+      if (screen.width > 781) {
             $(window).scroll(function () {
                   var height = $('body').height();
                   var scrollTop = $('body').scrollTop();
                   var opacity = 1;
-                  opacity = 1 - (scrollTop/600)
+                  opacity = 1 - (scrollTop/400)
+                  $('#top-title').css('opacity', opacity);
+            });
+      } else if(screen.width > 480) {
+            $(window).scroll(function () {
+                  var height = $('body').height();
+                  var scrollTop = $('body').scrollTop();
+                  var opacity = 1;
+                  opacity = 1 - (scrollTop/50)
                   $('#top-title').css('opacity', opacity);
             });
       } else {
@@ -12,7 +20,7 @@
                   var height = $('body').height();
                   var scrollTop = $('body').scrollTop();
                   var opacity = 1;
-                  opacity = 1 - (scrollTop/40)
+                  opacity = 1 - (scrollTop/20)
                   $('#top-title').css('opacity', opacity);
             });
       }
