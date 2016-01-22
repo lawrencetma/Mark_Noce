501f75e38eb54a990676a69d3af230ca12f05c1e
Lawrence Ma <lawrencema@lawrences-air.attlocal.net>
2016-01-21 22:15:15 -0800

    removes parallax effect from homepage so that it does not conflict with newsletter js

--- app/assets/javascripts/adjustStyle.js
+++ app/assets/javascripts/adjustStyle.js
@@ -1,21 +1,21 @@
-function adjustStyle(width) {
-  width = parseInt(width);
-  if (width <= 1024) {
-    $("#homepage-background").addClass('background').removeClass('parallax');
-  } else {
-     $("#homepage-background").addClass('parallax').removeClass('background');
-  }
-}
+// function adjustStyle(width) {
+//   width = parseInt(width);
+//   if (width <= 1024) {
+//     $("#homepage-background").addClass('background').removeClass('parallax');
+//   } else {
+//      $("#homepage-background").addClass('parallax').removeClass('background');
+//   }
+// }
 
-$(document).ready(function() {
-  adjustStyle($(this).width());
-  $(window).resize(function() {
-    adjustStyle($(this).width());
-  });
-});
-$(document).on('page:load', function() {
-  adjustStyle($(this).width());
-  $(window).resize(function() {
-    adjustStyle($(this).width());
-  });
-});
+// $(document).ready(function() {
+//   adjustStyle($(this).width());
+//   $(window).resize(function() {
+//     adjustStyle($(this).width());
+//   });
+// });
+// $(document).on('page:load', function() {
+//   adjustStyle($(this).width());
+//   $(window).resize(function() {
+//     adjustStyle($(this).width());
+//   });
+// });
